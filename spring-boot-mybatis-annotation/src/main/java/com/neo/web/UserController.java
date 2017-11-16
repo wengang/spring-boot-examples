d5ef89bdd9ccf49d514c93001f6889fb7d2447cf
Yuwg <tao-yu@hotmail.com>
2017-11-16 15:24:59 +0800

    fixed add method on controller can not work's issue

--- spring-boot-mybatis-annotation/src/main/java/com/neo/web/UserController.java
+++ spring-boot-mybatis-annotation/src/main/java/com/neo/web/UserController.java
@@ -3,9 +3,7 @@ package com.neo.web;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.neo.entity.UserEntity;
 import com.neo.mapper.UserMapper;
@@ -16,29 +14,29 @@ public class UserController {
 	@Autowired
 	private UserMapper userMapper;
 	
-	@RequestMapping("/getUsers")
+	@RequestMapping(value="/getUsers",method = RequestMethod.GET)
 	public List<UserEntity> getUsers() {
 		List<UserEntity> users=userMapper.getAll();
 		return users;
 	}
 	
-    @RequestMapping("/getUser")
+    @RequestMapping(value = "/getUser", method = RequestMethod.GET)
     public UserEntity getUser(Long id) {
     	UserEntity user=userMapper.getOne(id);
         return user;
     }
     
-    @RequestMapping("/add")
-    public void save(UserEntity user) {
+    @RequestMapping(value = "/add",method = RequestMethod.POST)
+    public void save(@RequestBody UserEntity user) {
     	userMapper.insert(user);
     }
     
-    @RequestMapping(value="update")
+    @RequestMapping(value="update",method = RequestMethod.PUT)
     public void update(UserEntity user) {
     	userMapper.update(user);
     }
     
-    @RequestMapping(value="/delete/{id}")
+    @RequestMapping(value="/delete/{id}",method = RequestMethod.DELETE)
     public void delete(@PathVariable("id") Long id) {
     	userMapper.delete(id);
     }
